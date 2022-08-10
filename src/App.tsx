d35e6e98cb28b287ad79d4af9c3cd169bbd902f9
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-11 00:51:07 +0800

    Import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Activity from "./components/Activity";
 import Profile from "./components/Profile";
 
 function App() {
   return (
-    <main className="App bg-fem-700 min-h-screen flex items-center justify-center selection:bg-fem-950 selection:text-fem-700 font-rubik">
-      <div className="">
+    <main className="App bg-fem-700 min-h-screen flex items-center justify-center selection:bg-fem-950 selection:text-fem-700 font-rubik text-neutral-50">
+      <div className="grid grid-cols-4">
         <Profile />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
       </div>
     </main>
   );
