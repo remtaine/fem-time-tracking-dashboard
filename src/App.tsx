32235ea5f13eab23792d7f1fc06b9d4807797c01
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-12 01:13:52 +0800

    Remove unneeded components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Activity from "./components/Activity";
+import ActivityDashboard from "./components/ActivityDashboard";
 import Profile from "./components/Profile";
 
 function App() {
   return (
-    <main className="App bg-fem-700 min-h-screen flex items-center justify-center selection:bg-fem-950 selection:text-fem-700 font-rubik text-neutral-50">
-      <div className="grid grid-cols-4">
+    <main className="App bg-fem-700 min-h-screen flex items-center justify-center selection:bg-neutral-200 selection:text-neutral-900 font-rubik text-neutral-50">
+      <div className="flex gap-6">
         <Profile />
-        <Activity />
-        <Activity />
-        <Activity />
-        <Activity />
-        <Activity />
-        <Activity />
+        <ActivityDashboard />
       </div>
     </main>
   );
