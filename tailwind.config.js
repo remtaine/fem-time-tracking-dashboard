a02e007bde7cc6530820984a0001f37dbb215c67
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-12 01:14:05 +0800

    Add custom font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       xs: "475px",
       ...defaultTheme.screens,
     },
+    fontSize: {
+      "2xs": ".6rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
     extend: {
       colors: {
         fem: {
@@ -17,6 +30,8 @@ module.exports = {
           300: "hsl(348, 100%, 68%)",
           400: "hsl(145, 58%, 55%)",
           500: "hsl(264, 64%, 52%)",
+          550: "#5746EA",
+          575: "#34397B",
           600: "hsl(43, 84%, 65%)",
           700: "hsl(226, 43%, 10%)",
           800: "hsl(235, 46%, 20%)",
