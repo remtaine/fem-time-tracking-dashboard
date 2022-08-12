1a2a24bd84e4f91e14ead5b02fab220dd70b4299
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-12 10:56:04 +0800

    Add hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ActivityDashboard from "./components/ActivityDashboard";
 import Profile from "./components/Profile";
 
-function App() {
+function App(prop: any) {
+  // const [active, setActive] = useState("Weekly");
+  const { timePeriod } = prop;
+  const changeActive = function () {};
   return (
     <main className="App bg-fem-700 min-h-screen flex items-center justify-center selection:bg-neutral-200 selection:text-neutral-900 font-rubik text-neutral-50">
       <div className="flex gap-6">
