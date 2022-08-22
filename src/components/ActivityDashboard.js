65bc1b6699100405220c1f3ed66f073d68c3aff8
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-22 15:05:38 +0800

    Add context

--- src/components/ActivityDashboard.js
+++ src/components/ActivityDashboard.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Activity from "./Activity";
 import PropTypes from "prop-types";
+import { Context } from "../App";
 
 function ActivityDashboard(prop) {
-  const { timePeriod } = prop;
+  const { timePeriod, setTimePeriod } = useContext(Context);
   const data = require("../data/data.json");
 
+  console.log(timePeriod);
   console.log(data);
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 gap-4">
@@ -26,8 +28,8 @@ function ActivityDashboard(prop) {
           return (
             <Activity
               title={datum["title"]}
-              current={datum["timeframes"][timePeriod].current}
-              past={datum["timeframes"][timePeriod].previous}
+              current={datum["timeframes"][timePeriod.toLowerCase()].current}
+              past={datum["timeframes"][timePeriod.toLowerCase()].previous}
               // past={datum["timeframes"]}
               key={index}
             />
--- src/components/ui/TimePeriodButton.js
+++ src/components/ui/TimePeriodButton.js
@@ -4,15 +4,19 @@ import { Context } from "../../App";
 
 function TimePeriodButton(props) {
   const { text } = props;
+  const { timePeriod, setTimePeriod } = useContext(Context);
 
+  // console.log(user);
   return (
     <button
       className={
         "hover:text-neutral-50 duration-400 text-lg" +
         " " +
-        (true ? "text-neutral-50" : "text-fem-900")
+        (timePeriod === text ? "text-neutral-50" : "text-fem-900")
       }
-      onClick={() => {}}
+      onClick={() => {
+        setTimePeriod(text);
+      }}
     >
       {text}
     </button>
